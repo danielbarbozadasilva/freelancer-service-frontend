efdb1355a89ab3417df8901a6be4bea1281e18a0
Daniel Barboza da Silva <daniel.bsilva@al.infnet.edu.br>
2024-01-01 06:05:27 -0300

    feat: authenticated routes

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/portal/home'
 import Error404 from './pages/error/404'
+import Error403 from './pages/error/403'
 import Layout from './components/layout/main'
 import SignIn from './pages/portal/auth/signin'
 import SignUp from './pages/portal/auth/signup'
+
+const isAuthenticated = ()=>{
+  return true
+}
 
 export default function MainRoutes() {
   const router = createBrowserRouter([
@@ -18,7 +23,26 @@ export default function MainRoutes() {
         },
         {
           path: 'SignIn',
-          element: <SignIn />
+          element: isAuthenticated()? <SignIn /> : <Error403 title="Ocorreu um erro" />
+        },
+        {
+          path: 'SignUp',
+          element: <SignUp />
+        }
+      ]
+    },
+    {
+      path: 'dashboard',
+      element: <Layout />,
+      errorElement: <Error404 title="Página não encontrada" />,
+      children: [
+        {
+          path: 'home',
+          element: <Home />
+        },
+        {
+          path: 'SignIn',
+          element: isAuthenticated()? <SignIn /> : <Error403 title="Ocorreu um erro" />
         },
         {
           path: 'SignUp',
